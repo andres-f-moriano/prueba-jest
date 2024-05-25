e3e64fdd278c12fdd2873059649c8f555e691709
Andres_f_moriano <andres2004moriano@gmail.com>
2024-05-24 22:01:55 -0500

    test jest adios mundo

--- src/03-capitalizar/capi.test.js
+++ src/03-capitalizar/capi.test.js
@@ -3,8 +3,8 @@ const capitalizeText = require("./capi");
 describe("tests en la función para capitalizar textos", () => {
     
     test("test para verificar la conversión de una cadena a título ", () => {
-        const result = capi("hola chanchito feliz");
-        expect(result).toBe("Hola Chanchito Feliz");
+        const result = capi("adios mundo");
+        expect(result).toBe("adios mundo");
     })
 
     test("test para comprobar que una cadena vacía devuelva una cadena vacía", () => {
@@ -13,7 +13,7 @@ describe("tests en la función para capitalizar textos", () => {
     })
 
     test("test para asegurar que una cadena ya en capitalizada no cambie ", () => {
-        const result = capi("Hello Beautiful World");
-        expect(result).toBe("Hello Beautiful World");
+        const result = capi("bay to a World");
+        expect(result).toBe("bay to a wordl");
     })
 });
